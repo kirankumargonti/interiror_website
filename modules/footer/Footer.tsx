e14b6d3b584b6835e67b23dc202db0213ec0b1fb
mounikahalde <hmounika1997@gmail.com>
2022-06-10 22:19:57 +0530

    social links added

--- modules/footer/Footer.tsx
+++ modules/footer/Footer.tsx
@@ -1,10 +1,53 @@
-import React from "react"
+import React, { useId } from "react"
+import { FaFacebookF, FaLinkedinIn } from "react-icons/fa"
+import { AiOutlineTwitter } from "react-icons/ai"
+import { BsInstagram } from "react-icons/bs"
 
 const Footer = () => {
+  const socialArray = [
+    {
+      id: useId(),
+      icon: <FaFacebookF />,
+      link: "",
+    },
+    {
+      id: useId(),
+      icon: <AiOutlineTwitter />,
+      link: "",
+    },
+    {
+      id: useId(),
+      icon: <BsInstagram />,
+      link: "",
+    },
+    {
+      id: useId(),
+      icon: <FaLinkedinIn />,
+      link: "",
+    },
+  ]
+
   return (
     <footer>
-      <div className='footer'>
-        @copyright 2022 bn interior - All Rights Received
+      <div className='container'>
+        <div className='footer_wrapper'>
+          <div className='footer_top'>
+            {socialArray?.map((network) => (
+              <a
+                href={network?.link}
+                className='icon'
+                target='_blank'
+                key={network?.id}
+              >
+                {network?.icon}
+              </a>
+            ))}
+          </div>
+          <div className='footer_bottom'>
+            @copyright {new Date().getFullYear()} BN INTERIOR - All Rights
+            Received
+          </div>
+        </div>
       </div>
     </footer>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 //Components
-import Footer from '../modules/footer/Footer'
-import Header from '../modules/header/Header'
-import Hero from '../modules/hero/Hero'
+import Footer from "../modules/footer/Footer"
+import Header from "../modules/header/Header"
+import Hero from "../modules/hero/Hero"
 
 const index = () => {
   return (
     <>
-      <Header/>
+      <Header />
       <Hero />
       <Footer />
     </>

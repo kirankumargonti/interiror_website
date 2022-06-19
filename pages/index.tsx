472b6be03fc817164645782802cef9082e6dd560
mounikahalde <hmounika1997@gmail.com>
2022-06-19 20:30:24 +0530

    testimonial

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,14 @@
 import Footer from "../modules/footer/Footer"
 import Header from "../modules/header/Header"
 import Hero from "../modules/hero/Hero"
+import Testimonial from "../modules/testimonial/Testimonial"
 
 const index = () => {
   return (
     <>
       <Header />
       <Hero />
+      <Testimonial />
       <Footer />
     </>
   )
